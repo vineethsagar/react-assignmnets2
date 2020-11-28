5820f61af6fe28a71369a54c7bad1d37944bf19b
vineethsagar <vineethsagar.molla@gmail.com>
2020-11-28 17:46:50 +0530

    react - practice intro,class component,Virtual DOM, JSX,Immutability,Element,Component

---
+++ practice-1/src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+import React,{Component} from "react";
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      property : 0
+    }
+    this.handleClick= this.handleClick.bind(this);
+  }
+
+  // changing state
+  handleClick(){
+    this.setState((perviousState)=>{
+      return {property: perviousState.property +1};
+    })
+  }
+
+  componentDidMount(){
+    console.log("Component Mounted : increment");
+  }
+
+  componentDidUpdate(){
+    console.log("Component Updated :increment");
+  }
+  render(){
+    return(
+      <>
+        <h1>Increment : {this.state.property}</h1>
+        <button onClick={this.handleClick}>Change</button>
+        {console.log("rendered : increment")}
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ practice-1/src/decrement.jsx
@@ -0,0 +1,48 @@
+import React,{Component} from "react";
+
+class Decrement extends Component{
+
+    constructor(props){
+        super(props);
+        this.state= {
+            property : 1
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    //called before component is rendered. initialize values from props
+    static  getDerivedStateFromProps(props,state){
+        // console.log(props)
+        //console.log(state)
+
+
+        return {property: Number(props.value)}
+    }
+
+    handleClick = ()=>{
+            this.setState((previousState)=>{
+            return {property : previousState.property -1};
+        })
+    }
+
+    // called after component is rendered
+    componentDidMount(){
+        console.log("Called immediately after a component is mounted. Setting state here will trigger re-rendering")
+    }
+
+    
+    componentDidUpdate(){
+        console.log("Called immediately after updating occurs. Not called for the initial render.")
+    }
+
+    render(){
+        return(
+        <>
+            <h1> Decrement : {this.state.property}</h1>
+            <button onClick= {this.handleClick}>Decrement</button>
+            {console.log("re-rendered  decrement")}
+        </>
+        )
+    }
+}
+export default Decrement;
